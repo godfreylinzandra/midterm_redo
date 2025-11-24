882e3a8903e57d8896911b96fbd342b4894ed0ce
Godfreylin Zandra Gapud <gapudgodfreylinz@gmail.com>
2025-11-24 16:25:29 +0800

    Deploy website

--- finals.sql
+++ finals.sql
@@ -1,1 +1,13 @@
-select * from users
+SELECT 
+    t.id AS transaction_id,
+    t.user_id,
+    u.name AS user_name,
+    u.email AS user_email,
+    t.category,
+    t.note,
+    t.amount,
+    t.date
+FROM transactions t
+JOIN users u
+  ON t.user_id = u.id
+ORDER BY t.date DESC;
